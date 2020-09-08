8c58f80a9b523207340b2ab2b7e534fe74043a9c
Dariya <dariia.vakhitova@icloud.com>
2020-09-09 00:51:31 +0300

    Solved task from lab4

--- week4/ex2.c
+++ week4/ex2.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-int main(){
-	for(int i=0;i<5;i+=1){
-		fork();
-	}
-	sleep(5);
+int main() {
+  for (int i = 0; i < 5; i += 1) {
+    fork();
+  }
+  sleep(5);
 }
