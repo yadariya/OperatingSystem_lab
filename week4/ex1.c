d41a94a3460f0b2f7b727a79477e07da42c0aa3f
Dariya <dariia.vakhitova@icloud.com>
2020-09-08 23:58:35 +0300

    Solved task from lab4

---
+++ week4/ex1.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+int main(){
+	int n=30;
+	pid_t pid=fork();
+	for(int i=0;i<n;i+=1){
+		if(pid!=0){
+		printf("Hello from parent %d %d\n", getpid(), i);}
+		else{
+    	printf("Hello from child %d %d\n", getpid(), i);}
+    }
+}
+
