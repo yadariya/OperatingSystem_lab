75351c5759af7544cc6fd14368651040d20f1f16
Dariya <dariia.vakhitova@icloud.com>
2020-09-09 13:52:45 +0300

    Solved task 3 from lab4

---
+++ week4/ex3.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include<string.h>
+ int main(){
+
+ 	char string[128];
+
+    while(1){
+        printf("> ");
+        scanf("%s", string);
+        system(string);
+    }
+    return 0;
+ }
