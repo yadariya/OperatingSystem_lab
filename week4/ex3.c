ddc3bf38b5651b19a41f9b691b7ffeb498145abd
Dariya <dariia.vakhitova@icloud.com>
2020-09-09 21:08:18 +0300

    solved task 3 from lab 4

--- week4/ex3.c
+++ week4/ex3.c
@@ -1,12 +1,16 @@
 #include <stdio.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include<string.h>
+#include <string.h>
+#include <stdlib.h>
  int main(){
  	char string[128];
  	while(1){
         printf("> ");
         scanf("%s", string);
+        if (strcmp(string,"exit")==0){
+        	break;
+        }
         system(string);
     }
     return 0;
